724046a2442e22c72b936847bfa60dc3089c9334
Mark Drzycimski <mark@clickrain.com>
2016-09-16 16:09:11 -0500

    Add a filename busting method.

--- bust/variables/BustVariable.php
+++ bust/variables/BustVariable.php
@@ -3,9 +3,26 @@ namespace Craft;
 
 class BustVariable
 {
-	public function er($file)
+	private var $availableMethods = array('querystring','filename');
+
+	public function er($file,$method="querystring")
 	{
-		$time = filemtime($_SERVER['DOCUMENT_ROOT'] . $file);
-		return $file . '?' . $time;
+		$availableMethods = array('querystring','filename');
+		$hash = substr(sha1(filemtime($_SERVER['DOCUMENT_ROOT'] . $file)),0,8);
+
+		if ( in_array($method, $availableMethods) ) {
+			switch ($method) {
+				case 'querystring':
+					return $file . '?' . $hash;
+
+				case 'filename':
+					$lastpos = strrpos($file, '.');
+					$left = substr($file, 0, $lastpos + 1);
+					$right = substr($file, $lastpos);
+					return $left . $hash . $right;
+			}
+		}
+
+		return $file;
 	}
 }
